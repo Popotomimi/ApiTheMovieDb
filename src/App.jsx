9337973321a6870869d1f98168dd16bf21f68b39
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2023-09-18 20:20:43 -0300

    1
    1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+import './App.css'
+import { Outlet } from 'react-router-dom';
+
+// Components
+import Navbar from './components/Navbar';
+
+function App() {
+
+  return (
+    <div className='App'>
+      <Navbar />
+      <Outlet />
+    </div>
+  )
+}
+
+export default App;
